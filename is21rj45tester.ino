b225c00146468cb44e2aff36fd26ccaa87dd81f1
Zunder45 <sashaorlov02y@yandex.ru>
2021-11-12 23:59:13 +0500

    add display

--- is21rj45tester.ino
+++ is21rj45tester.ino
@@ -1,3 +1,5 @@
+#include <LiquidCrystal_I2C.h>
+
 int pinOrange = 3;
 int pinWOrange = 2;
 int pinGreen = 5;
@@ -7,7 +9,10 @@ int pinWBlue = 6;
 int pinBrown = 9;
 int pinWBrown = 8;
 
-int pin[] = {pinOrange,
+int tonePin = 12;
+
+int pin[] = {
+             pinOrange,
              pinGreen,
              pinBlue,
              pinBrown
@@ -18,8 +23,11 @@ int wpin[] = {
                pinWBlue,
                pinWBrown
 };
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
-//  Serial.begin(9600);/
+  Serial.begin(9600);
   
   pinMode(0, INPUT_PULLUP);
   
@@ -28,48 +36,61 @@ void setup() {
   pinMode(pinBlue, OUTPUT);
   pinMode(pinBrown, OUTPUT);
 
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
-//  Serial.print(sizeof(pin));
+  pinMode(tonePin, OUTPUT);
+
+  lcd.begin();
+  lcd.backlight();
+  lcd.home();
+  lcd.print("Pins:12345678");
+
 
 }
 
 void loop() {
 
-  if(digitalRead(0)){
+  if(digitalRead(11)){
     while(true){
-      if(!digitalRead(0)){
+      if(!digitalRead(11)){
          break;
       }
     }
+    clearLcd();
     for(int i = 0; i < 4; i++){
        digitalWrite(pin[i], HIGH);
+//       if(digitalRead(wpin[i])){
+//          writeLcd(wpin[i],String(wpin[i], DEC));
+//          beep(3440,30);
+//       }
+//       else{
+//          writeLcd(wpin[i], "X"); 
+//          beep(440,300);
+//       } 
        if(digitalRead(pinWOrange)){
           beep(3440,30);
-          led(10,500);
+          writeLcd(wpin[i],String(pinWOrange, DEC));
        }
        else if(digitalRead(pinWGreen)){
           beep(3440,30);
-          led(11,500);
+          writeLcd(wpin[i],String(pinWGreen, DEC));
           
        }
        else if(digitalRead(pinWBlue)){
           beep(3440,30);
-          led(12,500);
+          writeLcd(wpin[i],String(pinWBlue, DEC));
        }
        else if(digitalRead(pinWBrown)){
           beep(1318.5,50);
-          led(13,500); 
+          writeLcd(wpin[i],String(pinWBrown, DEC));
        }
        else{
-          beep(440,300); 
-          delay(30);
+          writeLcd(wpin[i], "X"); 
+          beep(440,300);
        }
        digitalWrite(pin[i], LOW);
+       delay(500);
     }
   }
+  
 }
 
 void led(int n, int speed){
@@ -78,9 +99,20 @@ void led(int n, int speed){
       digitalWrite(n, LOW);
       delay(speed/2);
 }
+void clearLcd(){
+  lcd.clear();
+  lcd.print("Pins:12345678");
+}
+
+void writeLcd(int x1, String ch){
+  int position = 4+x1;
+  lcd.setCursor(position,1);
+  Serial.print(position);
+  lcd.print(ch);
+}
 
 void beep(int h, int dly){
-  tone(1,h);
+  tone(tonePin,h);
   delay(dly);
-  noTone(1);
+  noTone(tonePin);
 }
